182275df1fc78a4957d95d282dc0958005f783bb
diuguide <everett.diuguid@gmail.com>
2022-08-25 22:01:56 -0400

    created a main reducer

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,9 +1,10 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
-
+import mainReducer from '../features/slice/mainSlice';
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    mainSlice: mainReducer
   },
 });
 
